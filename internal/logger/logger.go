c4bb5025523bfdff35c0c095c6fc1f52e422ed4c
MKirsanov.PC <kir.commersion@internet.ru>
2025-02-10 13:40:10 +0500

    add config_loader

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,20 @@
 package logger
+
+import (
+	"log"
+	"os"
+)
+
+type Logger struct {
+	Err  *log.Logger
+	Info *log.Logger
+}
+
+// InitLogger - метод инициализирующий логирование
+func InitLogger() *Logger {
+	logger := Logger{}
+	logger.Info = log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
+	logger.Err = log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime)
+	logger.Info.Print("Executing InitLogger.")
+	return &logger
+}
