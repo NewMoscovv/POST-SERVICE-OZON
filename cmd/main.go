21d21ccd9628f511537af68805efa3dff8d365bc
MKirsanov.PC <kir.commersion@internet.ru>
2025-02-10 15:24:16 +0500

    start server, add websocket support

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/websocket"
+	resolvers "my_app/internal/server/graphql"
+	"time"
+
 	"my_app/internal/config"
+	gfql "my_app/internal/graphql"
 	"my_app/internal/logger"
+	"net/http"
 	"os"
 )
 
@@ -20,4 +29,29 @@ func main() {
 	if err := config.Init(envfile); err != nil {
 		lgr.Err.Fatal(err.Error())
 	}
+
+	port := os.Getenv("PORT")
+
+	srv := handler.New(gfql.NewExecutableSchema(gfql.Config{Resolvers: &resolvers.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	// добавляем поддержку WebSocket
+	srv.AddTransport(&transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
+
+	// добавляем поддержку других транспортов
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+
+	lgr.Info.Printf("Connect to http://localhost:%s/ for GraphQL playground", port)
+	lgr.Err.Fatal(http.ListenAndServe(":"+port, nil))
 }
